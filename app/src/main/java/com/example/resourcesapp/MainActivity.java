a66d61bb586976ed344a3c18961ae59b83659a17
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-28 12:26:15 -0400

    creando menus

--- app/src/main/java/com/example/resourcesapp/MainActivity.java
+++ app/src/main/java/com/example/resourcesapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +21,26 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Java
-        Resources res = getResources();
-        int color = res.getColor(R.color.purple_700);
-
-
+        /*Resources res = getResources();
+        int color = res.getColor(R.color.purple_700);*/
 
     }
+
+    // How create menus
+
+    // Java
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+
+    // Kotlin
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean{
+        menuInflater.inflate(R.menu.my_menu, menu)
+                return true
+    }*/
+
 }
